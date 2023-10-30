44c2a0b69c33e86abe80156debfd9332245a9a26
Danil <danil_soldatenko_99@mail.ru>
2023-10-30 21:59:36 +0800

    first

---
+++ components/Library.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Library = () => {
+    const onClick = () => {
+        //Handle upload later
+    }
+    return (
+        <div>
+            Libraray!
+        </div>
+    );
+};
+
+export default Library;
