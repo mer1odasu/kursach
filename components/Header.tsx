5081c6691a03ef69c7a5b2ddb0495af4b0b3e47b
Danil <danil_soldatenko_99@mail.ru>
2023-11-03 10:59:36 +0300

    Header 1

---
+++ components/Header.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import {useRouter} from "next/navigation";
+import {twMerge} from "tailwind-merge";
+
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string
+}
+
+const Header: React.FC<HeaderProps> = ({
+    children,
+    className,
+}) => {
+    const router = useRouter()
+
+    const handleLogout = () => {
+        // Logout
+    }
+
+
+    return (
+        <div
+        className={twMerge(`
+        h-fit
+        bg-gradient-to-b
+        from-emerald-800
+        p-6
+        `,
+            className
+        )}
+        >
+            <div className="
+            w-full
+            mb-4
+            flex
+            items-center
+            justify-between
+            " >
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
